6b4c80b027fd596ba37a74ceb929025129f7fcf9
Nel <nelsonsalazarm93@gmail.com>
2020-07-30 22:51:05 -0300

    prueba con Annyang

--- assets/functions.js
+++ assets/functions.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo')
+if (annyang) {
+    var commands = {
+      'show tps report': function() {
+        alert("Hola mundo!");
+      }
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+  }
+
